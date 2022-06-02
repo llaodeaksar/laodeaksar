e2891176a0b835688a63cbb797c8eac543462cc2
llaodeaksar <laodeaksarr@gmail.com>
2022-06-02 08:53:00 +0000

    add global styles

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import { globalStyles } from "design-system";
 import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+//import '../styles/font.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  globalStyles()
+
   return <Component {...pageProps} />
 }
 
