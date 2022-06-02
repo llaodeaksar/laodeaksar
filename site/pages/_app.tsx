dc3166ef9e6b5659ddf9a006c7c9c797606cf445
llaodeaksar <laodeaksarr@gmail.com>
2022-06-02 16:27:29 +0000

    add useDebouncedValue hooks

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,4 +1,5 @@
-import { globalStyles } from "design-system";
+import Head from 'next/head';
+import { globalStyles, ThemeProvider } from "design-system";
 import type { AppProps } from 'next/app'
 import '../styles/globals.css';
 //import '../styles/font.css';
@@ -6,7 +7,19 @@ import '../styles/globals.css';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   globalStyles()
 
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta
+          name="google-site-verification"
+          content="f11boUvGIzjbYwQVuaCieN-J4vcA_BxJuO_S54WPf-U"
+        />
+      </Head>
+      {/* <DefaultSeo />*/}
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
