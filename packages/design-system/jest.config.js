a225332808e3dee06e4a2174331a42e7a9cfff68
llaodeaksar <laodeaksarr@gmail.com>
2022-06-03 15:04:02 +0000

    edit

--- packages/design-system/jest.config.js
+++ packages/design-system/jest.config.js
@@ -1,4 +1,33 @@
+/*module.exports = {
+    modulePaths: ['node_modules', '<rootDir>'],
+    setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+    testEnvironment: 'jsdom',
+    testPathIgnorePatterns: ['/node_modules/'],
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'esbuild-jest',
+    },
+};*/
+
 module.exports = {
-    ...require('config/jest-design'),
-    rootDir: '.',
-}
+    verbose: true,
+    testEnvironment: 'jsdom',
+    roots: ['<rootDir>/src'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+        '!src/test/**',
+        '!src/index.tsx',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
+};
