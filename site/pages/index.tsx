702a45e8b7beb96573221de10e2c8ec1d1937a18
llaodeaksar <laodeaksarr@gmail.com>
2022-06-03 08:17:51 +0000

    add layout

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
-import { Button } from 'design-system'
+import { Button,Grid } from 'design-system'
+import Layout from '@theme/layout';
 
 const Home: NextPage = () => {
   return (
-    <Button variant='primary'>Test</Button>
+    <Layout /*footer header headerProps={{ offsetHeight: 256 }}*/>
+      <Grid columns="medium" gapX={4} gapY={12} all>
+        <Button variant='primary'>Test</Button>
+      </Grid>
+    </Layout>
   )
 }
 
